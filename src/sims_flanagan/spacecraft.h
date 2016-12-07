531d5712a3a2035a022e4483e0d21d16848c64d8
Louis Dufour <lf.dufour@gmail.com>
2016-12-07 14:51:47 +0100

    Added support for get_thrustElectricSolar
    Support thrust function of power - return the max available power as a function of the distance to the sun in AU. The ISP is considered constant.

--- src/sims_flanagan/spacecraft.h
+++ src/sims_flanagan/spacecraft.h
@@ -27,6 +27,7 @@
 #define KEP_TOOLBOX_SPACECRAFT_H
 
 #include <iostream>
+#include <algorithm>    // std::max
 
 // Serialization code
 #include "../serialization.h"
@@ -48,10 +49,29 @@ class __KEP_TOOL_VISIBLE spacecraft
 {
 	friend std::ostream &operator<<(std::ostream &s, const spacecraft &in );
 public:
-	spacecraft():m_mass(0),m_thrust(0),m_isp(0) {}
-	spacecraft(const double &mass_, const double &thrust_, const double &isp_) : m_mass(mass_), m_thrust(thrust_), m_isp(isp_) {}
+	spacecraft():m_mass(0),m_thrust(0),m_isp(0), m_AT(0), m_BT(0), m_maxP(0), m_minP(0), m_Pmargin(0) {}
+	spacecraft(const double &mass_, const double &thrust_, const double &isp_) 
+		: m_mass(mass_), m_thrust(thrust_), m_isp(isp_), m_AT(0), m_BT(0), m_maxP(0), m_minP(0), m_Pmargin(0) {}
+	spacecraft(const double &mass_, const double &thrust_, const double &isp_,
+		  const double &AT_, const double &BT_, const double &maxP_,const double &minP_,
+		  const double &P1AU_, const double &Pmargin_) 
+		: m_mass(mass_),m_thrust(thrust_),m_isp(isp_), m_AT(AT_), m_BT(BT_), m_maxP(maxP_), m_minP(minP_), m_Pmargin(Pmargin_) {}
 	double get_mass() const {return m_mass;}
 	double get_thrust() const {return m_thrust;}
+	double get_thrust_electricSolar(double distanceSun) const {
+		double P = std::max(m_P1AU/(distanceSun-1.0)/(distanceSun - 1.0)-m_Pmargin,0.0);
+		double thrustElectric = 0.0;
+		if(P<=m_minP){
+			thrustElectric = 0.0;
+			return thrustElectric;
+		}else if(P>m_maxP){
+			P = m_Pmax;
+		}else{
+			P = P;
+		}
+		thrustElectric  = m_AT + m_BT*P;
+		return thrustElectric ; 
+	}
 	double get_isp() const {return m_isp;}
 	void set_mass(const double _mass) {m_mass=_mass;}
 	void set_thrust(const double _thrust) {m_thrust=_thrust;}
@@ -66,11 +86,23 @@ private:
 		ar & m_mass;
 		ar & m_thrust;
 		ar & m_isp;
+		ar &  m_AT;
+		ar &  m_BT;
+		ar &  m_maxP;
+		ar &  m_minP;
+		ar &  m_P1AU;
+		ar &  m_Pmargin
 	}
 // Serialization code (END)
 	double m_mass;
 	double m_thrust;
 	double m_isp;
+	double m_AT;
+	double m_BT;
+	double m_maxP;
+	double m_minP;
+	double m_P1AU;
+	double m_Pmargin;
 };
 
 __KEP_TOOL_VISIBLE std::ostream &operator<<(std::ostream &s, const spacecraft &in );
